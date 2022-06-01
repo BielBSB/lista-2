918f87fd096994e796a5417187b322551a939f21
BielDuCalote <106703450+BielDuCalote@users.noreply.github.com>
2022-06-01 16:43:26 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,22 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 
- int main() {
+int main() {
 	
-	float valor,xerox;
+	float Dt,Cg,Cm;
 	
-	printf("Quantas xerox voce quer?\n");
-	scanf("%f",&xerox);
+	printf("Consumo medio\n");
 	
-	if(xerox>100){ 
-	valor= xerox*0.20;
-	printf("valor a ser pago e: %.2f",valor);
-	}else if(xerox > 0 && xerox<100){
-		valor= xerox*0.25;	
-		printf("valor a ser pago e: %.2f",valor);
-	} else{
-		printf("Numero de copias impossiveis.");
-	}
-
-
-
+	printf("Informe a distancia total percorrida: ");
+	scanf("%f",&Dt);
 	
+	printf("Informe a quantidade de combustivel gasto: ");
+	scanf("%f",&Cg);
+
+	Cm = Dt/Cg;	
+	
+	printf("O consumo medio e %.2f",Cm);
+
 	
 	return 0;
 }
-
